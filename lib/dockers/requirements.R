ef0a2cd8522ef06f3fe0e6324083d2b4bcdd82fa
benizar <benizar@gmail.com>
2021-03-17 20:58:15 +0100

    drafting

--- lib/dockers/requirements.R
+++ lib/dockers/requirements.R
@@ -1,3 +1,12 @@
 #install.packages("packagename")
 install.packages("bookdown")
 install.packages("tinytex")
+
+
+install.packages("infer")
+install.packages("moderndive")
+install.packages("kableExtra")
+install.packages("patchwork")
+install.packages("ggrepel")
+
+
